1202fc5f37a34df84ad006d5cf61dfa528124f29
Amartya Bintang Wijat Ranti <48303704+L200180193@users.noreply.github.com>
2020-04-22 15:58:41 +0700

    Add files via upload

---
+++ Modul7/No 1.py
@@ -0,0 +1,11 @@
+# No 1
+import re
+
+a = open("Indonesia.txt", 'r', encoding='latin1')
+
+text = a.read()
+a.close()
+x = r'me\w+'
+tampilkan = re.findall(x,text)
+
+print(tampilkan)
---
+++ Modul7/No 3.py
@@ -0,0 +1,11 @@
+# No 3
+import re
+
+a = open("Indonesia.txt", 'r', encoding='latin1')
+
+text = a.read()
+a.close()
+x = r'di \w+'
+tampilkan = re.findall(x,text)
+
+print(tampilkan)
